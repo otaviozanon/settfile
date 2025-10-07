e6c5f01a91d7249dcd06455bd6e883a24ec9887d
Otavio <otaviozanonn@icloud.com>
2025-10-07 02:18:32 -0300

    fix sort

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -19,32 +19,18 @@ export interface Provider {
 
 export const PROVIDERS: Provider[] = [
   {
-    id: "gofile",
-    name: "gofile.io",
-    maxMB: 10240, // 10 GB
-    expire: "7 days",
-    upload: uploadToGofile,
-  },
-  {
-    id: "tmpfiles",
-    name: "tmpfiles.org",
-    maxMB: 100, // 100 MB grátis
-    expire: "60 minutes",
-    upload: uploadToTmpfiles,
-  },
-  {
-    id: "catbox",
-    name: "catbox.moe",
-    maxMB: 200, // 200 MB
-    expire: "Indefinite",
-    upload: uploadToCatbox,
-  },
-  {
     id: "freeimage",
     name: "Freeimage.host",
     maxMB: 64, // 64 MB
     expire: "Indefinite",
     upload: uploadToFreeimage,
+  },
+  {
+    id: "tmpfiles",
+    name: "tmpfiles.org",
+    maxMB: 100, // 100 MB
+    expire: "60 minutes",
+    upload: uploadToTmpfiles,
   },
   {
     id: "filebin",
@@ -54,10 +40,24 @@ export const PROVIDERS: Provider[] = [
     upload: uploadToFilebin,
   },
   {
+    id: "catbox",
+    name: "catbox.moe",
+    maxMB: 200, // 200 MB
+    expire: "Indefinite",
+    upload: uploadToCatbox,
+  },
+  {
     id: "ufile",
     name: "Ufile.io",
     maxMB: 5120, // 5 GB
     expire: "Indefinite",
     upload: uploadToUfile,
+  },
+  {
+    id: "gofile",
+    name: "gofile.io",
+    maxMB: 10240, // 10 GB
+    expire: "7 days",
+    upload: uploadToGofile,
   },
 ];
